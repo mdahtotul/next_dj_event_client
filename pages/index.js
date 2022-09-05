0a553c03d2e8e69a8702af7c379dac90be373473
arifulhasantotul <mdahtotul@gmail.com>
2022-09-05 21:09:27 +0600

    get static props

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "../config";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout title="Home">
-      <h1>This is home page</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
