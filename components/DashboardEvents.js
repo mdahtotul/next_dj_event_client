0fbff0ec0a7eab9d6cb2f27e5f569695ab8036d0
arifulhasantotul <mdahtotul@gmail.com>
2022-09-20 11:36:07 +0600

    dashboard frontend init

---
+++ components/DashboardEvents.js
@@ -0,0 +1,33 @@
+import styles from "@/styles/DashboardEvent.module.css";
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+
+const DashboardEvents = ({ eventProps, handleDelete }) => {
+  console.log("evt", eventProps);
+
+  const { id, attributes } = eventProps;
+  const { name, slug, performers, venue, address, date, time, image } =
+    attributes;
+
+  return (
+    <div className={styles.event}>
+      <h4>
+        <Link href={`/events/${slug}`}>
+          <a>{name}</a>
+        </Link>
+      </h4>
+
+      <Link href={`/events/edit/${id}`}>
+        <a className={styles.edit}>
+          <FaPencilAlt /> <span>Edit Event</span>
+        </a>
+      </Link>
+
+      <a href="#" className={styles.delete} onClick={() => handleDelete()}>
+        <FaTimes /> <span>Delete</span>{" "}
+      </a>
+    </div>
+  );
+};
+
+export default DashboardEvents;
--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,14 +1,53 @@
+import DashboardEvents from "@/components/DashboardEvents";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 import { useAuthContext } from "@/context/AuthContext";
+import { parseCookies } from "@/helpers/index";
+import styles from "@/styles/Dashboard.module.css";
 
-const DashboardPage = () => {
+const DashboardPage = ({ events }) => {
   const { user } = useAuthContext();
+
+  console.log(events);
+
+  const deleteEvent = (id) => {
+    console.log("delete", id);
+  };
+
   return (
     <Layout title="Dashboard">
-      <p>{user?.username}</p>
-      <p>{user?.email}</p>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <p>{user?.username}</p>
+        <p>{user?.email}</p>
+        <h3>My Events</h3>
+      </div>
+      {events.map((event, idx) => (
+        <DashboardEvents
+          key={idx}
+          eventProps={event}
+          handleDelete={() => deleteEvent(event?.id)}
+        />
+      ))}
     </Layout>
   );
 };
 
 export default DashboardPage;
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/api/event/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: { events: events.data },
+  };
+}
